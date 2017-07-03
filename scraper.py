eaf91ff386f81c72f1b20f6b0dc34f8a7851dcde
Jay SyKo <jay@jaysyko.com>
2017-07-02 20:54:50 -0400

    Fixed formatting

--- scraper.py
+++ scraper.py
@@ -3,6 +3,8 @@
 import pandas as pd
 from scrapy import Spider
 from player import Player
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
 
 CENTER = "center"
 LEFT_WING = "left_wing"
@@ -22,32 +24,29 @@ class NHLScraper(Spider):
 
 	def parse(self, response):
 		rows = response.xpath("//table[3]/tr[starts-with(@class, 'ysprow')]")
+		parsed_url = urlparse(response.url)
+		parsed_query = parse_qs(parsed_url.query)
+		current_position = parsed_query['pos'][0]
 		for row in rows:
-			plyer = Player()
-			plyer["player_name"] = titles["player" + str(i)] = row.xpath("td[@class='yspscores'][1]/a/text()")[0].extract()
-			plyer["team"] = titles["player" + str(i)] = row.xpath("td[@class='yspscores']/a/text()")[1].extract()
-			plyer["position"] = current_position
-			plyer["games_played"] = titles["player" + str(i)] = row.xpath("td[@class='yspscores'][5]/text()").extract()
-			plyer["goals"] = titles["player" + str(i)] = row.xpath("td[@class='yspscores'][7]/text()").extract()
-			plyer["assists"] = titles["player" + str(i)] = row.xpath("td[@class='ysptblclbg6']/span/text()").extract()
-			plyer["points"] = titles["player" + str(i)] = row.xpath("td[@class='yspscores'][10]/text()").extract()
-			plyer["plus_minus"] = titles["player" + str(i)] = row.xpath("td[@class='yspscores'][12]/text()").extract()
-			plyer["pim"] = titles["player" + str(i)] = row.xpath("td[@class='yspscores'][14]/text()").extract()
-			plyer["hits"] = titles["player" + str(i)] = row.xpath("td[@class='yspscores'][16]/text()").extract()
-			plyer["bks"] = titles["player" + str(i)] = row.xpath("td[@class='yspscores'][18]/text()").extract()
-			plyer["fw"] = titles["player" + str(i)] = row.xpath("td[@class='yspscores'][20]/text()").extract()
-			plyer["fl"] = titles["player" + str(i)] = row.xpath("td[@class='yspscores'][22]/text()").extract()
-			plyer["fop"] = titles["player" + str(i)] = row.xpath("td[@class='yspscores'][24]/text()").extract()
-			plyer["ppg"] = titles["player" + str(i)] = row.xpath("td[@class='yspscores'][26]/text()").extract()
-			plyer["shg"] = titles["player" + str(i)] = row.xpath("td[@class='yspscores'][28]/text()").extract()
-			plyer["sha"] = titles["player" + str(i)] = row.xpath("td[@class='yspscores'][30]/text()").extract()
-			plyer["gw"] = titles["player" + str(i)] = row.xpath("td[@class='yspscores'][32]/text()").extract()
-			plyer["sog"] = titles["player" + str(i)] = row.xpath("td[@class='yspscores'][34]/text()").extract()
-			plyer["pct"] = titles["player" + str(i)] = row.xpath("td[@class='yspscores'][36]/text()").extract()
-			yield plyer
-
-def scrape():
-	x = NHLScraper()
-
-if __name__ == "__main__":
-	scrape()
+			current_player = Player()
+			current_player["name"] = row.xpath("td[@class='yspscores'][1]/a/text()")[0].extract()
+			current_player["team"] = row.xpath("td[@class='yspscores']/a/text()")[1].extract()
+			current_player["position"] = current_position
+			current_player["games_played"] = row.xpath("td[@class='yspscores'][5]/text()")[0].extract()
+			current_player["goals"] = row.xpath("td[@class='yspscores'][7]/text()")[0].extract()
+			current_player["assists"] = row.xpath("td[@class='ysptblclbg6']/span/text()")[0].extract()
+			current_player["points"] = row.xpath("td[@class='yspscores'][10]/text()")[0].extract()
+			current_player["plus_minus"] = row.xpath("td[@class='yspscores'][12]/text()")[0].extract()
+			current_player["pim"] = row.xpath("td[@class='yspscores'][14]/text()")[0].extract()
+			current_player["hits"] = row.xpath("td[@class='yspscores'][16]/text()")[0].extract()
+			current_player["bks"] = row.xpath("td[@class='yspscores'][18]/text()")[0].extract()
+			current_player["fw"] = row.xpath("td[@class='yspscores'][20]/text()")[0].extract()
+			current_player["fl"] = row.xpath("td[@class='yspscores'][22]/text()")[0].extract()
+			current_player["fop"] = row.xpath("td[@class='yspscores'][24]/text()")[0].extract()
+			current_player["ppg"] = row.xpath("td[@class='yspscores'][26]/text()")[0].extract()
+			current_player["shg"] = row.xpath("td[@class='yspscores'][28]/text()")[0].extract()
+			current_player["sha"] = row.xpath("td[@class='yspscores'][30]/text()")[0].extract()
+			current_player["gw"] = row.xpath("td[@class='yspscores'][32]/text()")[0].extract()
+			current_player["sog"] = row.xpath("td[@class='yspscores'][34]/text()")[0].extract()
+			current_player["pct"] = row.xpath("td[@class='yspscores'][36]/text()")[0].extract()
+			yield current_player
