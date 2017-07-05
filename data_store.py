261c4dc422cb49dc5eb42ce68bc4796db8892246
Jay SyKo <jay@jaysyko.com>
2017-07-04 20:40:55 -0400

    Basic Working

--- data_store.py
+++ data_store.py
@@ -2,6 +2,7 @@
 
 import sqlite3
 import pandas as pd
+
 DB_FILE = "player_stats.sqlite"
 TABLE_NAME = "stats"
 
@@ -16,15 +17,16 @@ class DataStore:
 			self.conn.commit()	
 
 	def save_value(self, item):
-		if item["fop"] == 'N/A':
-			item["fop"] = -9999
-		if item["pct"] == 'N/A':
-			item["pct"] = -9999
-		if '\'' in item["name"]:
-			item["name"] = item["name"].replace('\'', "")
-			
-		self.c.execute("INSERT INTO {tbl} (name, team, games_played, position, goals, assists, points, plus_minus, pim, hits, bks, fw, fl, fop, ppg, ppa, shg, sha, gw, sog, pct ) VALUES ( '{name}', '{team}', '{games_played}', '{position}', '{goals}', '{assists}', '{points}', '{plus_minus}', '{pim}', '{hits}', '{bks}', '{fw}', '{fl}', '{fop}', '{ppg}', '{ppa}', '{shg}', '{sha}', '{gw}', '{sog}', '{pct}' );".format(tbl=TABLE_NAME, name=str(item["name"]), team=item["team"], games_played=item["games_played"], position=item["position"], goals=item["goals"], assists=item["assists"], points=item["points"], plus_minus=item["plus_minus"], pim=item["pim"], hits=item["hits"], bks=item["bks"], fw=item["fw"], fl=item["fl"], fop=float(item["fop"]), ppg=item["ppg"], ppa=item["ppa"], shg=item["shg"], sha=item["sha"], gw=item["gw"], sog=item["sog"], pct=float(item["pct"])))
-		self.conn.commit()
+		if item["name"] != "Auston Matthews":
+			if item["fop"] == 'N/A':
+				item["fop"] = -99999
+			if item["pct"] == 'N/A':
+				item["pct"] = -99999
+			if '\'' in item["name"]:
+				item["name"] = item["name"].replace('\'', "")
+				
+			self.c.execute("INSERT INTO {tbl} (name, team, games_played, position, goals, assists, points, plus_minus, pim, hits, bks, fw, fl, fop, ppg, ppa, shg, sha, gw, sog, pct ) VALUES ( '{name}', '{team}', '{games_played}', '{position}', '{goals}', '{assists}', '{points}', '{plus_minus}', '{pim}', '{hits}', '{bks}', '{fw}', '{fl}', '{fop}', '{ppg}', '{ppa}', '{shg}', '{sha}', '{gw}', '{sog}', '{pct}' );".format(tbl=TABLE_NAME, name=str(item["name"]), team=item["team"], games_played=int(item["games_played"]), position=item["position"], goals=int(item["goals"]), assists=int(item["assists"]), points=int(item["points"]), plus_minus=int(item["plus_minus"]), pim=int(item["pim"]), hits=int(item["hits"]), bks=int(item["bks"]), fw=int(item["fw"]), fl=int(item["fl"]), fop=float(item["fop"]), ppg=int(item["ppg"]), ppa=int(item["ppa"]), shg=int(item["shg"]), sha=int(item["sha"]), gw=int(item["gw"]), sog=int(item["sog"]), pct=float(item["pct"])))
+			self.conn.commit()
 
 	def fetch_all(self):
 		query = "SELECT * FROM {tbl}".format(tbl=TABLE_NAME)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
+import numpy as np
 from data_store import DataStore
+from sklearn import preprocessing, model_selection, neighbors
 
 db_data = DataStore()
 
@@ -8,6 +10,20 @@ df = db_data.fetch_all()
 df.drop(['id'], 1, inplace=True)
 df.drop(['name'], 1, inplace=True)
 df.drop(['team'], 1, inplace=True)
-print(df)
-
 db_data.close()
+
+X = np.array(df.drop(['position'], 1))
+y = np.array(df['position'])
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+clf = neighbors.KNeighborsClassifier()
+clf.fit(X_train, y_train)
+
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
+
+# Predict Auston Matthews
+eg = np.array([82, 40, 29, 69, 2, 14, 21, 61, 530, 602, 0.468, 8, 13, 0, 0, 8, 279, 0.143])
+eg = eg.reshape(1, -1)
+prediction = clf.predict(eg)
+print(prediction)
